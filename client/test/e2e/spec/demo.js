06e012bc252d63380045a090fb24a9270be79a0d
alichen <ali322@gmail.com>
2018-02-07 14:33:00 +0800

    fix test-e2e spec

--- client/test/e2e/spec/demo.js
+++ client/test/e2e/spec/demo.js
@@ -1,6 +1,6 @@
 import { Selector } from 'testcafe'
 
-fixture('get started').page('http://127.0.0.1:9090/')
+fixture('get started').page('http://127.0.0.1:3000/')
 
 test('my first test', async t => {
   await t.expect(Selector('.event').count).gt(0)
