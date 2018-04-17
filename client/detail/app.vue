5fbdae84cce7651a691c6d097a25f54183609669
alichen <ali322@gmail.com>
2018-04-17 21:03:02 +0800

    fix pass

--- client/detail/app.vue
+++ client/detail/app.vue
@@ -1,42 +1,41 @@
 <template>
-<div class="container">
-    <div class="header header-with-btn"><button class="btn" @click="goBack"><i class="fa fa-arrow-left" /></button>{{detail.name}}</div>
-    <div class="content">
-        <div class="detail">
-            <img :src="detail.avatar" alt="" />
-            <p>Title: {{detail.title}}</p>
-            <p>Created at: {{detail.created_at}}</p>
-            <p>{{detail.message}}</p>
+    <div class="container">
+        <div class="header header-with-btn">
+            <button class="btn" @click="goBack">
+                <i class="fa fa-arrow-left" />
+            </button>{{detail.name}}</div>
+        <div class="content">
+            <div class="detail">
+                <img :src="detail.avatar" alt="" />
+                <p>Title: {{detail.title}}</p>
+                <p>Created at: {{detail.created_at}}</p>
+                <p>{{detail.message}}</p>
+            </div>
         </div>
     </div>
-</div>
 </template>
 
 <script>
-    import { mapState, mapActions } from 'vuex'
-    import * as actions from './action'
-    import detailStore from './store'
+import { mapState, mapActions } from 'vuex'
+import * as actions from './action'
 
-    export default {
-        name: 'Detail',
-        asyncState({store, route}) {
-            // store.registerModule('detail', detailStore)
-            return store.dispatch('fetchDetail', route.params.id)
-        },
-        computed: {
-            ...mapState({
-                detail: state => state.detail.detail,
-                route: 'route'
-            })
-        },
-        methods: {
-            goBack(){
-                window.history.back()
-            },
-            ...mapActions(Object.keys(actions))
-        },
-        mounted() {
-            this.fetchDetail(this.route.params.id)
-        }
-    }
+export default {
+  name: 'Detail',
+  asyncState({ store, route }) {
+    // store.registerModule('detail', detailStore)
+    return store.dispatch('fetchDetail', route.params.id)
+  },
+  computed: {
+    ...mapState({
+      detail: state => state.detail.detail,
+      route: 'route'
+    })
+  },
+  methods: {
+    goBack() {
+      window.history.back()
+    },
+    ...mapActions(Object.keys(actions))
+  }
+}
 </script>
--- client/index/app.vue
+++ client/index/app.vue
@@ -1,33 +1,36 @@
 <template>
-<div class="container">
-    <div class="header">Github Events1 <button class="refresh-btn refresh-icon" @click="handleRefresh"></button></div>
-    <div class="content">
-        <div class="event" v-for="event in events" :key="event.id">
-            <router-link :to="{path:'/detail/'+event.id}">
-                <div class="event-title">
-                    <img :src="event.avatar" alt="" />
-                    <span>
-                    <p>{{event.name}}</p>
-                    <p>{{event.created_at}}</p>
-                    </span>
-                    <i class="fa fa-caret-right" />
-                </div>
-                <p>{{event.type}} In <b>{{event.repo}}</b></p>
-            </router-link>
-        </div>
+  <div class="container">
+    <div class="header">Github Events1
+      <button class="refresh-btn refresh-icon" @click="handleRefresh"></button>
     </div>
-</div>
+    <div class="content">
+      <div class="event" v-for="event in events" :key="event.id">
+        <router-link :to="{path:'/detail/'+event.id}">
+          <div class="event-title">
+            <img :src="event.avatar" alt="" />
+            <span>
+              <p>{{event.name}}</p>
+              <p>{{event.created_at}}</p>
+            </span>
+            <i class="fa fa-caret-right" />
+          </div>
+          <p>{{event.type}} In
+            <b>{{event.repo}}</b>
+          </p>
+        </router-link>
+      </div>
+    </div>
+  </div>
 </template>
 <script>
-import { mapActions, mapState } from "vuex"
-import * as actions from "./action"
-import indexStore from './store'
+import { mapActions, mapState } from 'vuex'
+import * as actions from './action'
 
 export default {
-  name: "Index",
+  name: 'Index',
   asyncState({ store, route }) {
     //   store.registerModule('index', indexStore)
-      return store.dispatch('fetchEvents')
+    return store.dispatch('fetchEvents')
   },
   methods: {
     handleRefresh() {
@@ -41,7 +44,7 @@ export default {
         console.log('index', state.index)
       },
       events: state => state.index.events,
-      route: "route"
+      route: 'route'
     })
   }
 }
