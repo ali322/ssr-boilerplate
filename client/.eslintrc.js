b3489abb13d4b05075447176343ecf22906b0773
alichen <ali322@gmail.com>
2018-04-17 21:07:41 +0800

    fix pass

---
+++ client/.eslintrc.js
@@ -0,0 +1,31 @@
+
+module.exports = {
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint',
+    sourceType: 'module'
+  },
+  env: {
+    browser: true
+  },
+  // https://github.com/feross/standard/blob/master/RULES.md#javascript-standard-style
+  extends: [
+    'standard'
+  ],
+  plugins: [
+    'promise'
+  ],
+  'rules': {
+    'arrow-parens': 'off',
+    'generator-star-spacing': 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'space-before-function-paren': 'off',
+		'new-cap': 'off',
+    'space-before-function-paren': 'off',
+    'promise/always-return': 'off',
+    'promise/no-return-wrap': 'off',
+    'promise/no-callback-in-promise': 'off',
+    'promise/catch-or-return': 'off',
+    'promise/avoid-new': 'off'
+  }
+}
